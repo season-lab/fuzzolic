6338db148e3a78e405c724e3fddadce3ceb7178a
Emilio Coppa <ercoppa@gmail.com>
2020-01-20 12:21:09 +0100

    testcase driver

--- tests/driver.c
+++ tests/driver.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
 int simple_if(uint32_t);
 
@@ -13,30 +14,48 @@ int adcl(uint32_t);
 int adcw(uint16_t);
 int adcb(uint8_t);
 
+typedef enum TestcaseInputType {
+    VAR,
+    BUFFER,
+} TestcaseInputType;
+
 typedef struct Testcase {
     const char* name;
     int (*f)(uintptr_t);
+    TestcaseInputType input_type;
+    size_t            input_size;
 } Testcase;
 
 #define F(x) ((int (*)(uintptr_t))x)
 
+#define MAX_INPUT_SIZE 256
+uint8_t data[MAX_INPUT_SIZE] = {0};
+
 Testcase tests[] = {
-    {.name = "simple_if", .f = F(simple_if)},
+    {.name       = "simple_if",
+     .f          = F(simple_if),
+     .input_type = VAR,
+     .input_size = 8},
     //
-    {.name = "addq", .f = F(addq)},
-    {.name = "addl", .f = F(addl)},
+    {.name = "addq", .f = F(addq), .input_type = VAR, .input_size = 8},
+    {.name = "addl", .f = F(addl), .input_type = VAR, .input_size = 8},
     //
-    {.name = "adcq", .f = F(adcq)},
-    {.name = "adcl", .f = F(adcl)},
-    {.name = "adcw", .f = F(adcw)},
-    {.name = "adcb", .f = F(adcb)},
+    {.name = "adcq", .f = F(adcq), .input_type = VAR, .input_size = 8},
+    {.name = "adcl", .f = F(adcl), .input_type = VAR, .input_size = 8},
+    {.name = "adcw", .f = F(adcw), .input_type = VAR, .input_size = 8},
+    {.name = "adcb", .f = F(adcb), .input_type = VAR, .input_size = 8},
 };
 
 void foo(Testcase* t)
 {
-    uint64_t p;
-    read(0, &p, sizeof(p));
-    int r = t->f(p);
+    read(0, data, sizeof(t->input_size));
+    int r;
+    if (t->input_type == VAR) {
+        uint64_t p = *((uint64_t*)data);
+        r          = t->f(p);
+    } else {
+        r = t->f((uint64_t)data);
+    }
     printf("RESULT=%d\n", r);
 }
 
@@ -57,6 +76,7 @@ int main(int argc, char* argv[])
     size_t i, found = 0;
     for (i = 0; i < sizeof(tests) / sizeof(Testcase); i++) {
         if (strcmp(testcase_name, tests[i].name) == 0) {
+            assert(tests[i].input_size < MAX_INPUT_SIZE);
             foo(&tests[i]);
             found = 1;
             break;
