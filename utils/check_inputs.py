00790606d027a2b8b11e2f50b5d5de298ab82c89
Emilio Coppa <ercoppa@gmail.com>
2020-03-26 14:26:40 +0100

    utils

---
+++ utils/check_inputs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import glob
+import filecmp
+
+root_dir = sys.argv[1]
+
+unique_inputs = []
+for f in glob.glob(root_dir + "/*/*.dat"):
+    is_unique = True
+    for fu in unique_inputs:
+        if filecmp.cmp(f, fu):
+            is_unique = False
+            print("%s is equal to %s" % (f, fu))
+            break
+    if is_unique:
+        unique_inputs.append(f)
+
